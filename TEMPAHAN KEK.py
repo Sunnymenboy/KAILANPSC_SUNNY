d806188695cddb47e997190545b3eab38197be4c
Sunnymenboy <135795431+Sunnymenboy@users.noreply.github.com>
2023-06-07 12:35:13 +0800

    CONTOH RUJUKAN PROJEK TEMA RC JET

---
+++ TEMPAHAN KEK.py
@@ -0,0 +1,19 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\MyPc\AppData\Local\Programs\Python\Python39\def casher TEMPAHAN KEK T3.py
+HARGA KEK KEJU=RM40,MENTEGA=RM35,PELANGI=RM25,KOPI=RM15
+Masukkan tempahan untuk kek keju:2
+Masukkan tempahan untuk kek mentega:4
+Masukkan tempahan untuk kek pelangi:1
+Masukkan tempahan untuk kek kopi:2
+
+
+Tempahan anda ialah:
+2 kek keju
+4 kek mentega
+1 kek pelangi
+2 kek kopi
+
+ Jumlah harga untuk tempahan ialah RM 275
+>>> 
